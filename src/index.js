c902ebd832e2755b53b3b83e30697dba1b444d21
nm1008 <mallari08nikko@yahoo.com>
2023-08-28 01:49:14 +0800

    added a condition for levels per skill and render it on the page

--- src/index.js
+++ src/index.js
@@ -4,43 +4,43 @@ import "./index.css";
 
 const skills = [
     {
-        skill: "HTML + CSS",
+        name: "HTML + CSS",
         level: "intermediate",
         color: "#2662EA"
 
     },
     {
-        skill: "React",
+        name: "React",
         level: "beginner",
         color: "#60DAFB"
     },
     {
-        skill: "Bootstrap",
+        name: "Bootstrap",
         level: "intermediate",
-        color: "##EE84F33"
+        color: "lightgreen"
     },
     {
-        skill: "Git and GitHub",
+        name: "Git and GitHub",
         level: "intermediate",
         color: "#C3DCAF"
     },
     {
-        skill: "Javascript",
+        name: "Javascript",
         level: "intermediate",
         color: "#EFD81D"
     },
     {
-        skill: "Next JS",
+        name: "Next JS",
         level: "beginner",
         color: "brown"
     },
     {
-        skill: "Node JS",
+        name: "Node JS",
         level: "beginner",
         color: "green"
     },
     {
-        skill: "Tailwind",
+        name: "Tailwind",
         level: "intermediate",
         color: "pink"
     }
@@ -82,23 +82,44 @@ function App() {
   function SkillList() {
     return (
       <div className="skill-list">
-        <Skill name="React " emoji="💪" color="red" />
-        <Skill name="HTML+CSS " emoji="💪" color="orange" />
-        <Skill name="Bootstrap " emoji="💪" color="yellow" />
-        <Skill name="Node JS " emoji="💪" color="green" />
-        <Skill name="Next JS " emoji="💪" color="brown" />
-        <Skill name="Tailwind " emoji="💪" color="pink" />
+
+        {/* we used map so that when the obj-arr is looped it will return a new array - we didn't use forEach since it prints out every item inside the array */}
+       {skills.map((skill) => (
+
+        //added key for each obj it is a unique value - you can use the name of the obj
+        <Skill skillObj={skill} key={skill.name} />
+
+       ))}
+
+         {/* passing key to another component  as props  */}
+            {/* <Skill name="React " emoji="💪" color="red" />
+            <Skill name="HTML+CSS " emoji="💪" color="orange" />
+            <Skill name="Bootstrap " emoji="💪" color="yellow" />
+            <Skill name="Node JS " emoji="💪" color="green" />
+            <Skill name="Next JS " emoji="💪" color="brown" />
+            <Skill name="Tailwind " emoji="💪" color="pink" /> */}
       </div>
     );
   }
 
   function Skill(props) {
-    console.log(props);
+    console.log(props.skillObj.emoji);
+
+    let emoji;
+
+    if(props.skillObj.level === "beginner"){
+        emoji = <span>👶</span>
+    }else if(props.skillObj.level === "intermediate"){
+        emoji = <span>🔥</span>
+    }else if(props.skillObj.level === "advanced") {
+        emoji = <span>💪</span>
+    }
+
     return (
       <div className="skill">
-        <div className="skill" style={{ backgroundColor: props.color }}>
-          <span>{props.name}</span>
-          <span>{props.emoji}</span>
+        <div className="skill" style={{ backgroundColor: props.skillObj.color }}>
+          <span>{props.skillObj.name}</span>
+            {emoji}
         </div>
       </div>
     );
